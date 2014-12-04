44e0fe4011adbe68ed5bd1b9e2188b7ea3bf5248
alka180 <alka.chaudhary423@gmail.com>
2014-12-04 23:04:22 +0530

    Create text.c

---
+++ text.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+    if(x == 9);
+ 
+    scanf("%d", &n);
+ 
+    printf("Hello");
+     
+    switch(p) {
+        case 1:
+            dosomething();
+             
+        case 2:
+            dosomething();
+    }
+}
